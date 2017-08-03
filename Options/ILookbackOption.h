712d540e25196019621986543d43d6eb65a254d4
copflo <fcopin9@gmail.com>
2017-08-04 00:05:29 +0100

    Lookback options refactoring

---
+++ Options/ILookbackOption.h
@@ -0,0 +1,16 @@
+#ifndef ILookbackOption_h
+#define ILookbackOption_h
+
+#include "PathDependentOption.h"
+
+class ILookbackOption 
+    : public PathDependentOption
+{
+public:
+            ILookbackOption(Nature nature, int maturity, int obsFreq);
+    virtual ~ILookbackOption();
+
+    virtual std::string style() const;
+};
+
+#endif /* ILookbackOption_h */
--- Options/LookbackOption.cpp
+++ Options/LookbackOption.cpp
@@ -1,15 +1,65 @@
+#include <algorithm>
+
 #include "LookbackOption.h"
 
-LookbackOption::LookbackOption(Nature nature, int maturity, int obsFreq)
-    : PathDependentOption(nature, maturity, obsFreq)
+
+LookbackOption<FixedStrike>::LookbackOption(Option::Nature nature, int maturity, int obsFreq, double strike)
+    : ILookbackOption(nature, maturity, obsFreq)
+    , FixedStrike(strike)
 {
 }
 
-LookbackOption::~LookbackOption()
+LookbackOption<FixedStrike>::~LookbackOption()
 {
 }
 
-std::string LookbackOption::style() const
+double LookbackOption<FixedStrike>::payoff(const std::vector<double>& spot_path) const
 {
-    return "Lookback";
+    return Option::payoff(optimalSpot(spot_path), _strike);
+}
+
+void LookbackOption<FixedStrike>::print(std::ostream& os) const
+{
+    ILookbackOption::print(os);
+    os << "Strike: " << _strike << std::endl;
+}
+
+double LookbackOption<FixedStrike>::optimalSpot(const std::vector<double>& spot_path) const
+{
+    if (isCall()) {
+        return *std::max_element(spot_path.begin(), spot_path.end());
+    }
+    else {
+        return *std::min_element(spot_path.begin(), spot_path.end());
+    }
+}
+
+
+LookbackOption<FloatingStrike>::LookbackOption(Nature nature, int maturity, int obsFreq)
+    : ILookbackOption(nature, maturity, obsFreq)
+{
+}
+
+LookbackOption<FloatingStrike>::~LookbackOption()
+{
+}
+
+double LookbackOption<FloatingStrike>::payoff(const std::vector<double>& spot_path) const
+{
+    return Option::payoff(spot_path.back(), strike(spot_path));
+}
+
+double LookbackOption<FloatingStrike>::strike(const std::vector<double>& spot_path) const
+{
+    if (isCall()) {
+        return *std::min_element(spot_path.begin(), spot_path.end());
+    }
+
+    return *std::max_element(spot_path.begin(), spot_path.end());
+}
+
+void LookbackOption<FloatingStrike>::print(std::ostream& os) const
+{
+    ILookbackOption::print(os);
+    os << "Strike: floating" << std::endl;
 }
