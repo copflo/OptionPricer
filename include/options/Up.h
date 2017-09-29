615acddc1dc5ef6e7ff7393182635c45f7a07fa9
copflo <fcopin9@gmail.com>
2017-09-29 18:12:53 +0200

    Add method for checking if spot is beyond barrier

--- include/options/Up.h
+++ include/options/Up.h
@@ -5,11 +5,13 @@
 #include <vector>
 
 
-class Up 
+class Up
 {
 public:
+    static bool        barrierIsCrossed(double barrier, double spot);
     static bool        barrierIsCrossed(double barrier, const std::vector<double>& spot_path);
     static std::string toString();
 };
+
 
 #endif // UP_H
--- src/options/Up.cpp
+++ src/options/Up.cpp
@@ -1,10 +1,16 @@
 #include <algorithm>
+
 #include "Up.h"
 
-bool Up::barrierIsCrossed(double barrier, const std::vector<double>& spot_path) 
+
+bool Up::barrierIsCrossed(double barrier, double spot)
 {
-    return std::any_of(spot_path.begin(), spot_path.end(), 
-        [barrier](double spot) { return spot >= barrier; });
+    return barrier <= spot;
+}
+
+bool Up::barrierIsCrossed(double barrier, const std::vector<double>& spot_path)
+{
+    return std::any_of(spot_path.begin(), spot_path.end(), [barrier](double spot) { return barrierIsCrossed(barrier, spot); });
 }
 
 std::string Up::toString()
