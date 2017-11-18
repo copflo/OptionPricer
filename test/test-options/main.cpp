c704750e67cd3429f5664e23d8ca17e6c7a185ba
copflo <fcopin9@gmail.com>
2017-11-18 17:33:15 +0100

    Add unit tests

---
+++ test/test-options/main.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "options/BlackScholes.h"
+
+
+const double eps = 0.01;
+const double risklessRate = 0.05;
+const double spot = 100.;
+const BlackScholes _bs(Volatility(0.15, 365));
+
+TEST(BlackScholesTest, PriceEuropeanCall) {
+    const EuropeanOption eu_call(new Call, 90, 105.);
+    ASSERT_NEAR(1.52, _bs.price(risklessRate, spot, eu_call), eps);
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
