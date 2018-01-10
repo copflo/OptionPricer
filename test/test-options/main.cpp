7924d30954fe240dfcf8bd334e96df13949ccc90
copflo <fcopin9@gmail.com>
2018-01-11 00:33:50 +0100

    cmake files

--- test/test-options/main.cpp
+++ test/test-options/main.cpp
@@ -6,11 +6,11 @@
 const double eps = 0.01;
 const double risklessRate = 0.05;
 const double spot = 100.;
-const BlackScholes _bs(Volatility(0.15, 365));
+const BlackScholes bs(Volatility(0.15, 365));
 
 TEST(BlackScholesTest, PriceEuropeanCall) {
     const EuropeanOption eu_call(new Call, 90, 105.);
-    ASSERT_NEAR(1.52, _bs.price(risklessRate, spot, eu_call), eps);
+    ASSERT_NEAR(1.52, bs.price(risklessRate, spot, eu_call), eps);
 }
 
 
