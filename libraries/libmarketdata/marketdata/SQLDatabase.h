fc19f1a067cfaf6438ffcfc9f8333c08d93c80b2
copflo <fcopin9@gmail.com>
2018-03-30 14:30:35 +0200

    Remove files not ready for release

--- libraries/libmarketdata/marketdata/SQLDatabase.h
+++
@@ -1,9 +0,0 @@
-class SQLDatabase
-{
-public:
-    static const std::string ISIN_QUERY;
-    static const std::string INSERT_SECURITY_QUERY;
-    static const std::string INSERT_QUOTE_QUERY;
-    static const std::string INSERT_CURRENCY_QUERY;
-    static const std::string VOLATILITY_QUERY;
-}
